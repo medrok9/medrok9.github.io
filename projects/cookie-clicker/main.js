36e7471831a6a016071df6ad058a14fc844e73d9
medr0k <143861386+medrok9@users.noreply.github.com>
2024-09-23 12:42:26 -0700

    Update main.js

--- projects/cookie-clicker/main.js
+++ projects/cookie-clicker/main.js
@@ -1163,19 +1163,13 @@ Game.Launch=function()
 	
 	Game.Load=function()
 	{
-		//l('javascriptError').innerHTML='<div style="padding:64px 128px;"><div class="title">Loading...</div></div>';
+	
 		Game.Loader=new Loader();
 		Game.Loader.domain='img/';
 		Game.Loader.loaded=Game.Init;
 		Game.Loader.Load(['filler.png']);
 	}
-	Game.ErrorFrame=function()
-	{
-		l('javascriptError').innerHTML=
-		'<div class="title">Oops. Wrong address!</div>'+
-		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
-		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
-		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
+	
 	}
 	Game.timedout=false;
 	Game.Timeout=function()
@@ -1189,7 +1183,7 @@ Game.Launch=function()
 	}
 	Game.Resume=function()
 	{
-		l('javascriptError').innerHTML='';
+	
 		l('javascriptError').style.display='none';
 		Game.timedout=false;
 		Game.time=Date.now();
